f7fa681c83e78a359da17aa2bdbe21568fad3221
abhinav sharma <abhiut37@gmail.com>
2020-12-10 15:44:34 +0530

    added few changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = 10000 || process.env.PORT;
+const PORT = 11000 || process.env.PORT;
 const path = require("path");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
